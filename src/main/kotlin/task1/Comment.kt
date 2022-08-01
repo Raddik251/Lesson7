e87404ddddbab2b02ec0a508b2c1c064db6dceaa
Raddik251 <raddik@inbox.ru>
2022-08-01 19:56:12 +0300

    Create task 1 for Lesson 7 (Exceptions)

--- src/main/kotlin/task1/Comment.kt
+++ src/main/kotlin/task1/Comment.kt
@@ -1,4 +1,17 @@
 package task1
 
-class Comment {
+import java.time.LocalDateTime
+
+class Comment (
+    val id:Int,
+    val fromId:Int?,
+    val date:LocalDateTime,
+    val text:String,
+    val donutComment: donutComment?,
+    val replyToUser:Int?,
+    val replyToComment:Int?,
+    val attachment: Attachment?,
+    val parentsStack:Array<Int>?,
+    val thread: task1.Thread?
+        ){
 }
--- src/main/kotlin/task1/WallService.kt
+++ src/main/kotlin/task1/WallService.kt
@@ -1,34 +1,52 @@
 package task1
 
-class WallService (posts:Array<Post>){
+class WallService(posts: Array<Post>) {
     protected var posts = emptyArray<Post>()
 
-    fun add (post:Post) {
+    fun add(post: Post) {
         posts += post
     }
 
-    fun update (postIn:Post) {
+    fun update(postIn: Post) {
         var a = 0
         for ((index, post) in posts.withIndex()) {
             if (post.id == postIn.id) {
                 posts[index] = postIn
                 a = 1
-                }
             }
+        }
         if (a === 0) {
-           println("Пост не найден")
+            println("Пост не найден")
         } else {
             println("Пост обновлен")
         }
     }
 
-    fun showLast (): Post {
+    fun showLast(): Post {
         return posts.last()
     }
 
-    fun checkUpdate (postIn: Post): Boolean {
+    fun checkUpdate(postIn: Post): Boolean {
         return if (postIn in posts) {
             return true
         } else false
+    }
+
+    private var comments = emptyArray<Comment>()
+    fun createComment(postId: Int, comment: Comment): Comment {
+        var b = 0
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == postId) {
+                comments += comment
+                b = 1
+            }
         }
+        if (b != 0) {
+            return comments.last()
+        }
+        throw PostNotFoundException ("Пост, на который ссылается комментарий не найден")
+    }
+}
+
+class PostNotFoundException(s: String) : Throwable() {
 }
